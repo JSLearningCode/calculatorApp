29a1fc1db782589835d438ba50784feec9b3859d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-03-29 13:20:00 -0300

    created the calculator logic

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
-let result = 0;
+const { getCurrentWindow } = require('electron').remote;
 
-let resultLabel = document.getElementById('result-label');
+var result = 0;
+var opStack;
 
 function clickElement(el) {
+    let resultLabel = document.getElementById('result-label');
+
     if (typeof(el) === 'number') {
-        resultLabel.innerText = el;
-    } 
+        if (opStack) {
+            switch(opStack) {
+                case '+':
+                    result += el;
+                    opStack = undefined;
+                    break;
+                case '-':
+                    result -= el;
+                    opStack = undefined;
+                    break;
+                case 'x':
+                    result *= el;
+                    opStack = undefined;
+                    break;
+                case '/':
+                    result /= el;
+                    opStack = undefined;
+                    break;
+            }
+        } else {
+            result = el;
+        }
+
+        resultLabel.innerText = result;
+        
+    } else {
+        if (el == ',') {
+            resultLabel.innerText = `${result}.`
+        } else if (el == '=') {
+            resultLabel.innerText = result;
+        } else {
+            opStack = el;
+            resultLabel.innerText = el;
+        }
+        
+    }
+        
 }
 
